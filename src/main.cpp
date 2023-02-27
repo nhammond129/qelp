28a05066fc14523b764bcb95581230318f271b12
Nicholas Hammond <nhammond129@gmail.com>
2023-02-27 02:48:54 -0600

    cull ShipEntity class

--- src/main.cpp
+++ src/main.cpp
@@ -11,34 +11,6 @@
 #include <string>
 #include <util.hpp>
 
-class ShipEntity: public sf::Sprite {
-public:
-    ShipEntity(const sf::Texture& texture, sf::Sprite&& attached_sprite, sf::Vector2f offset): mAttached(attached_sprite), mOffset(offset) {
-        setTexture(texture);
-        setOrigin(sf::Vector2f((float)(texture.getSize().x / 2), (float)(texture.getSize().y / 2)));
-        mAttached.setPosition(offset);
-    }
-    virtual ~ShipEntity() = default;
-
-    void pointAttachmentAt(sf::Vector2f target) {
-        sf::Vector2f offset = target - mAttached.getPosition();
-        float angle_rad = std::atan2(offset.y, offset.x);
-        mAttached.setRotation(sf::radians(angle_rad));
-    }
-
-    void update(float dt) {
-        mAttached.setPosition(getPosition() + mOffset.rotatedBy(getRotation()));
-    }
-
-    sf::Sprite mAttached;
-    sf::Vector2f mOffset;
-private:
-    void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override {
-        target.draw(static_cast<const sf::Sprite>(*this), states);
-        target.draw(mAttached, states);
-    }
-};  // class ShipEntity
-
 int main(int argc, char* argv[]) {
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
@@ -47,7 +19,6 @@ int main(int argc, char* argv[]) {
     }
 
     scenes::SceneManager manager(new scenes::MenuScene());
-
     while (manager.isRunning()) {
         manager.handleEvents();
         manager.update();
