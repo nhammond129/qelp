1fad9c7474747e642fb77e2772065d9544f5d6d0
Nicholas Hammond <nhammond129@gmail.com>
2023-03-11 02:21:58 -0600

    "comment out" some argc/argv debug for now

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,13 @@
 #include <util.hpp>
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+#if 0
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
             std::cout << "argv[" << i << "] = '"<< argv[i] << "'" << std::endl;
         }
     }
+#endif
 
     scenes::SceneManager manager(sf::VideoMode({config::SCREEN_WIDTH, config::SCREEN_HEIGHT}), config::WINDOW_TITLE);
     manager.pushScene(new scenes::MenuScene(manager));
