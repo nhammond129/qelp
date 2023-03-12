e2a834c7485993ccee953188ceeb7705d031bbd0
Nicholas Hammond <nhammond129@gmail.com>
2023-03-12 03:03:18 -0500

    cut test impl

--- src/main.cpp
+++ src/main.cpp
@@ -12,42 +12,7 @@
 #include <string>
 #include <util.hpp>
 
-#ifdef __linux__
-#include <unistd.h>
-#elif __APPLE__
-#include <mach-o/dyld.h>    // Not sure if this is correct. I don't have a mac to test this on.
-#elif _WIN32
-#include <Windows.h>
-#endif
-
-namespace aaa {
-
-const size_t PATH_MAX = 4096;
-const std::string executable_path() {
-#ifdef __linux__
-    char result[PATH_MAX];
-    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
-    return std::string(result, (count > 0) ? count : 0);
-#elif __APPLE__
-    char result[PATH_MAX];
-    uint32_t size = sizeof(result);
-    if (_NSGetExecutablePath(result, &size) == 0)
-        return std::string(result);
-    else
-        return std::string();
-#elif _WIN32
-    char result[PATH_MAX];
-    if (!GetModuleFileNameA(NULL, result, PATH_MAX))
-        throw std::runtime_error("Error getting executable path: " + std::to_string(GetLastError()));
-    return std::string(result);
-#endif
-}
-
-};  // namespace aaa
 int main() {
-
-
-    std::cout << "Executable path: " << aaa::executable_path() << std::endl;
 
     scenes::SceneManager manager(sf::VideoMode({config::SCREEN_WIDTH, config::SCREEN_HEIGHT}), config::WINDOW_TITLE);
     manager.pushScene(new scenes::MenuScene(manager));
