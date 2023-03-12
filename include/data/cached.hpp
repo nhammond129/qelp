7cc3710fe85ac0488304bd811299242897a2cd04
Nicholas Hammond <nhammond129@gmail.com>
2023-03-12 01:39:48 -0600

    resource cache WIP

---
+++ include/data/cached.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <filesystem>
+#include <SFML/Graphics.hpp>
+#ifdef __linux__
+#include <unistd.h>
+#elif __APPLE__
+#include <mach-o/dyld.h>    // Not sure if this is correct. I don't have a mac to test this on.
+#elif _WIN32
+#include <Windows.h>
+#endif
+
+namespace {
+
+const size_t PATH_MAX = 4096;
+const std::string executable_path() {
+#ifdef __linux__
+    char result[PATH_MAX];
+    ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+    return std::string(result, (count > 0) ? count : 0);
+#elif __APPLE__
+    char result[PATH_MAX];
+    uint32_t size = sizeof(result);
+    if (_NSGetExecutablePath(result, &size) == 0)
+        return std::string(result);
+    else
+        return std::string();
+#elif _WIN32
+    char result[PATH_MAX];
+    if (!GetModuleFileNameA(NULL, result, PATH_MAX))
+        throw std::runtime_error("Error getting executable path: " + std::to_string(GetLastError()));
+    return std::string(result);
+#endif
+}
+
+};  // namespace anonymous
+namespace data {
+
+template <typename T>
+struct Cache {
+    using Ptr = std::shared_ptr<T>;
+    using Map = std::unordered_map<std::string, Ptr>;
+
+    Map mCache;
+
+    const T& load(const std::string& path) {
+        auto it = mCache.find(path);
+        if (it != mCache.end())
+            return *(it->second);
+        Ptr ptr = std::make_shared<T>();
+        if (!ptr->loadFromFile(executable_path() + "/../" + path))
+            throw std::runtime_error("Error loading file: " + path);
+        mCache[path] = ptr;
+        return *ptr;
+    }
+
+    const T& load(const std::filesystem::path& path) {
+        return load(path.string());
+    }
+
+    void clear() {
+        mCache.clear();
+    }
+
+    const T& operator[](const std::string& path) {
+        return load(path);
+    }
+};
+
+Cache<sf::Texture> Textures;
+Cache<sf::Font> Fonts;
+
+};  // namespace data
